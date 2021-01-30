f751fc02dd9872a5f54af18cfd74af501ef2389b
Stefanie Nader <stefanie.nader1992@web.de>
2021-01-30 22:07:38 +0100

    second version

---
+++ script.js
@@ -0,0 +1,26 @@
+let fields = [];
+
+let currentShape = 'cross';
+
+function fillShape(id) {
+    if (currentShape == 'cross') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
+    }
+
+    fields[id] = currentShape;
+    draw();
+}
+
+function draw() {
+    for(i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+    }
+}
